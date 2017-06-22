eafb6d6f7f7ba35800b6c2c732090cc29bbd3a49
kedarguy <kedarguy@gmail.com>
2017-06-22 12:04:00 +0300

    framework for email app

--- src/components/EMail.vue
+++ src/components/EMail.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="email">
     <h1>Hello Email</h1>
+    <email-nav> </email-nav>
+    <div class="main">
+      <email-list> </email-list>
+      <email-preview> </email-preview>
+    </div>
   </div>
 </template>
 
 <script>
+import EmailNav from '@/components/EmailNav'
+import EmailList from '@/components/EmailList'
+import EmailPreview from '@/components/EmailPreview'
 
 export default {
   name: 'email-comp',
+  components: {
+    EmailNav,
+    EmailList,
+    EmailPreview
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .email {
-    background-color: purple;
-  }
+.email {
+  background-color: purple;
+}
 </style>
